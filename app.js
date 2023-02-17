730739a90f4e5246f5b28ef7b4571e73deac878c
Catherine Crenshaw <55335367+ccrenshawcodes@users.noreply.github.com>
2023-02-17 15:43:28 -0700

    Correct variable storage and display logic
    Changed how val1 and val2 are populated on line 68 so that when a new
    number was entered (intended as val2) after an operation, it was not
    just +='ed to val1.
    
    Added logic to clear val2 when operate() finishes running.
    Updated ops to match its new value after operate() runs.

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 //buttons
 const clear = document.querySelector('#clear');
-const equ = document.querySelector('#equals');
-
-const plus = document.querySelector('#plus');
-const minus = document.querySelector('#minus');
-const times = document.querySelector('#times');
-const div = document.querySelector('#div');
+const next = document.querySelectorAll('#next');
 
 const numbers = document.querySelectorAll('.numbers > .num');
 const operators = document.querySelectorAll('.operators > .ops');
-
-const allBtns = document.querySelectorAll('.btns');
 
 let display = document.querySelector('.display');
 
@@ -20,6 +13,9 @@ const calcValues = {
     ops: '',
 }
 
+let val1 = calcValues.val1;
+let val2 = calcValues.val2;
+let ops = calcValues.ops;
 
 //basic maths>>>
 function add(a, b) {
@@ -35,59 +31,71 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    if (b !== 0) {
-        return a / b;
-    } else {
-        return 'why are you trying to break my code? it wont work';
-    }
+    return a / b;
 }
+
 
 //'operate' function >>>
 function operate(n1, n2, operator) {
     if (operator === '+') {
         display.textContent = add(n1, n2);
+        val1 = add(n1, n2);
     }
     if (operator === '-') {
         display.textContent = subtract(n1, n2);
+        val1 = subtract(n1, n2);
     }
     if (operator === 'x') {
         display.textContent = multiply(n1, n2);
+        val1 = multiply(n1, n2);
     }
     if (operator === '÷') {
-        display.textContent = divide(n1, n2);
+        if (n2 === 0) {
+            display.textContent = 'why are you trying to break my code';
+        } else if (n2 !== 0) {
+            display.textContent = divide(n1, n2);
+            val1 = divide(n1, n2);
+        }
+        
     }
+    
 }
 
 
 //get & display number vals
+
+
 numbers.forEach(btn => {
     btn.addEventListener('click', () => {
-        if (!calcValues.ops) {
-            calcValues.val1 += btn.textContent;
-            display.textContent = calcValues.val1;
-        } else if (calcValues.val1 && calcValues.ops) {
-            calcValues.val2 += btn.textContent;
-            display.textContent = calcValues.val2;
+        if (val1 && !val2) {
+            val2 += btn.textContent;
+            display.textContent = val2;
+            //console.log(`second: ${val2}`);
+        } else {
+            val1 += btn.textContent;
+            display.textContent = val1;
+            //console.log(`first: ${val1}`);
         }
     })
 })
 
-//get operator
-operators.forEach(btn => {
-    btn.addEventListener('click', () => {
-        calcValues.ops = btn.textContent;
-    })
-})
 
-//perform operate with the correct vals
-equ.addEventListener('click', () => {
-    operate(Number(calcValues.val1), Number(calcValues.val2), calcValues.ops);
+next.forEach(btn => {
+    btn.addEventListener('click', () => {
+        
+        if (val1 && val2 && ops) {
+            operate(Number(val1), Number(val2), ops); 
+            val2 = '';
+            console.log(`When btn was clicked just now: val1: ${val1}. Val2: ${val2}. ops: ${ops}`);
+        }
+        ops = btn.textContent;
+    })
 })
 
 //clear all
 clear.addEventListener('click', () => {
-    calcValues.val1 = '';
-    calcValues.val2 = '';
-    calcValues.ops = '';
+    val1 = '';
+    val2 = '';
+    ops = '';
     display.textContent = '';
 })
